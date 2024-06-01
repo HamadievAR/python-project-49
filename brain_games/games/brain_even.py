34e9e340d1a48b62cadbf02b21a8de7f75c38d71
HamadievAR <rymphz@gmail.com>
2024-06-01 04:58:28 +0300

    refactor: brain_even.py improve maintainability

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -2,6 +2,21 @@ import random
 from brain_games.cli import welcome_user
 
 
+def generate_question():
+    number = random.randint(1, 100)
+    return number
+
+
+def check_answer(number, user_answer):
+    correct_answer = "yes" if number % 2 == 0 else "no"
+    if user_answer == correct_answer:
+        return True
+    else:
+        print(f"'{user_answer}' is wrong answer ;(. "
+              f"Correct answer was '{correct_answer}'.")
+        return False
+
+
 def main():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
@@ -10,19 +25,15 @@ def main():
     correct_answers_count = 0
 
     while correct_answers_count < correct_answers_needed:
-        number = random.randint(1, 100)
+        number = generate_question()
         print(f"Question: {number}")
         user_answer = input("Your answer: ").strip().lower()
 
-        correct_answer = "yes" if number % 2 == 0 else "no"
-
         if user_answer == "yes" or user_answer == "no":
-            if user_answer == correct_answer:
+            if check_answer(number, user_answer):
                 print("Correct!")
                 correct_answers_count += 1
             else:
-                print(f"'{user_answer}' is wrong answer ;(. "
-                      f"Correct answer was '{correct_answer}'.")
                 print(f"Let's try again, {name}!")
                 return
         else:
